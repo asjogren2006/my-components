35092370b7ac98d1eb3f649c96a607b41252e30e
Andrew Sjogren <908953@H2WHK1B8Q6NW.tld>
2022-10-18 09:39:26 -0500

    added dark mode

---
+++ nav/script.js
@@ -0,0 +1,10 @@
+function dark() {
+    document.getElementById("navbar").style.background = "#0082A3";
+    document.getElementById("buttons").style.color = "#DFDFDF";
+    document.getElementById("buttons1").style.color = "#DFDFDF";
+    document.getElementById("buttons2").style.color = "#DFDFDF";
+    document.getElementById("buttons3").style.color = "#DFDFDF";
+    document.getElementById("buttons4").style.color = "#DFDFDF";
+    document.getElementById("logo").style.color = "#DFDFDF";
+    
+}
